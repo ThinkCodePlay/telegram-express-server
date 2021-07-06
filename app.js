b866068287194f69577da6d97992258cbed2d706
kravitzy <82439353+ThinkCodePlay@users.noreply.github.com>
2021-07-06 22:34:04 +0300

    make it better

---
+++ app.js
@@ -0,0 +1,49 @@
+const { gameDeal } = require("./utils/cheapshark");
+
+const TelegramBot = require("node-telegram-bot-api");
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: false });
+
+const express = require("express");
+const port = 3000;
+
+const app = express();
+app.use(express.json());
+
+app.post("/bot", async (req, res) => {
+  const chat_id = req.body.message.chat.id;
+  const text = req.body.message.text;
+  return sendText(res, chat_id, `Hi there I got your message: ${text}`);
+});
+
+function sendText(res, chat_id, text) {
+  return res.status(200).send({
+    method: "sendMessage",
+    chat_id,
+    text: text,
+  });
+}
+
+// bot.onText(/\/game (.+)/, async (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const gameName = match[1];
+
+//   const deal = await gameDeal(gameName);
+//   bot.sendMessage(chatId, deal);
+// });
+
+// bot.on('message', (msg) => {
+//     console.log(msg);
+//     const chatId = msg.chat.id;
+
+//     // send a message to the chat acknowledging receipt of their message
+//     bot.sendMessage(chatId, 'Received your message');
+//   });
+
+// app.get('/bot', function (req, res) {
+//   res.send('Hello World')
+// })
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
---
+++ utils/cheapshark.js
@@ -0,0 +1,23 @@
+const axios = require("axios").default;
+
+const gameDeal = async (gameName) => {
+  try {
+    const response = await axios.get(
+      `https://www.cheapshark.com/api/1.0/games?title=${gameName}`
+    );
+
+    if (response.data.length > 0) {
+      console.log(response.data[0]);
+      const gameData = `Game name: ${response.data[0].external}. \nCheapest price found: ${response.data[0].cheapest}. \nDeal Link: https://www.cheapshark.com/redirect?dealID=${response.data[0].cheapestDealID}`;
+      return gameData;
+    } else {
+      return "no deals found";
+    }
+  } catch (error) {
+    return "Sorry, got an error";
+  }
+};
+
+module.exports = {
+  gameDeal,
+};
